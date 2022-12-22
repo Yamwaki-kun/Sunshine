d158276f466eb9f3998011c2a233e3288c97c661
Arnaldo Victor Yamawaki Alves <arnaldoalves34@gmail.com>
2022-12-21 21:19:46 -0300

    Criada tela de mensagens e sua respectiva model

---
+++ lib/model/MensagemChat.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class MensagemChat {
+  String conteudoMsg = "";
+  String tipoMsg = "";
+
+  MensagemChat(String conteudoMsgParam, String tipoMsgParam)
+  {
+    this.conteudoMsg = conteudoMsgParam;
+    this.tipoMsg = tipoMsgParam;
+  }
+
+
+}
--- lib/view/mensagens_page.dart
+++ lib/view/mensagens_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:sunshine_app/model/MensagemChat.dart';
 
 class MensagensPage extends StatefulWidget {
   const MensagensPage({super.key});
@@ -8,9 +10,134 @@ class MensagensPage extends StatefulWidget {
   State<MensagensPage> createState() => _MensagensPageState();
 }
 
+List<MensagemChat> msgList = <MensagemChat>[
+  MensagemChat("Olá", "Envio"),
+  MensagemChat("Olá", "Recebimento"),
+  MensagemChat("Posso alugar mais 2 horas", "Envio"),
+  MensagemChat("Pode sim!", "Recebimento"),
+];
+
 class _MensagensPageState extends State<MensagensPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.white,
+          flexibleSpace: SafeArea(
+            child: Container(
+              padding: EdgeInsets.only(right: 16),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 2,
+                  ),
+                  CircleAvatar(
+                    backgroundImage: NetworkImage(
+                        "https://avatars.githubusercontent.com/u/65573808?v=4"),
+                    maxRadius: 20,
+                  ),
+                  SizedBox(
+                    width: 12,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Vitor Amaral",
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w600),
+                        ),
+                        SizedBox(
+                          height: 6,
+                        ),
+                        Text(
+                          "Online",
+                          style: TextStyle(
+                              color: Colors.grey.shade600, fontSize: 13),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        body: Stack(children: <Widget>[
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.only(left: 10,bottom: 10,top: 10),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  SizedBox(width: 15,),
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: "Digite a sua mensagem",
+                          hintStyle: TextStyle(color: Colors.black54),
+                          border: InputBorder.none
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15,),
+                  FloatingActionButton(
+                    onPressed: (){},
+                    child: Icon(Icons.send,color: Colors.white,size: 18,),
+                    backgroundColor: Color.fromARGB(255, 98, 81, 248),
+                    elevation: 0,
+                  ),
+                ],
+
+              ),
+            ),
+          ),
+          ListView.builder(
+            itemCount: msgList.length,
+            shrinkWrap: true,
+            padding: EdgeInsets.only(top: 10, bottom: 10),
+            physics: NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Container(
+                padding:
+                    EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+                child: Align(
+                  alignment: (msgList[index].tipoMsg == "Recebimento"
+                      ? Alignment.topLeft
+                      : Alignment.topRight),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: (msgList[index].tipoMsg == "Recebimento"
+                          ? Colors.grey.shade700
+                          : Color.fromARGB(255, 98, 81, 248)),
+                    ),
+                    padding: EdgeInsets.all(16),
+                    child: Text(
+                      msgList[index].conteudoMsg,
+                      style: TextStyle(fontSize: 15, color: Colors.white),
+                    ),
+                  ),
+                ),
+              );
+            },
+          )
+        ]));
   }
 }
